c8d7a10c53ac90d9c96c99ecb904accb7ea0f002
Dmitriev Denis <den.dmitriev.2017@inbox.ru>
2023-11-22 14:05:23 +0300

    add loging and test request

--- DmitrievDenisKt-31-20/Program.cs
+++ DmitrievDenisKt-31-20/Program.cs
@@ -1,23 +1,38 @@
+using NLog;
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+try {
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch(Exception ex) {
+    logger.Error(ex, "Stopped programm because of exception");
+}
+finally {
+    LogManager.Shutdown();
 }
 
-app.UseAuthorization();
 
-app.MapControllers();
-
-app.Run();
